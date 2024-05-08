f6bf2f1c4b9567590bcbe2b97cb7cf2890dc054d
iolite312 <leonwatertor18@gmail.com>
2024-05-08 13:33:45 +0200

    Added the DAL layer template + base dao

---
+++ ChapeauDAL/ExampleDao.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace ChapeauDAL
+{
+    public class ExampleDao : baseDao
+    {
+        public int GetAllExample()
+        {
+            string query = "Here your query";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+        private int ReadTables(DataTable dataTable)
+        {
+            int count = 0;
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                count++;
+            }
+            return count;
+        } 
+    }
+}
---
+++ ChapeauDAL/baseDao.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ChapeauDAL
+{
+    public class baseDao
+    {
+        private SqlDataAdapter _adapter;
+        private SqlConnection _conn;
+
+        public baseDao()
+        {
+            _conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ChapeauDatabase"].ConnectionString);
+            _adapter = new SqlDataAdapter();
+        }
+
+        protected SqlConnection OpenConnection()
+        {
+            try
+            {
+                if (_conn.State == ConnectionState.Closed || _conn.State == ConnectionState.Broken)
+                {
+                    _conn.Open();
+                }
+            }
+            catch (Exception e)
+            {
+                //Print.ErrorLog(e);
+                throw;
+            }
+            return _conn;
+        }
+
+        private void CloseConnection()
+        {
+            _conn.Close();
+        }
+
+        /* For Insert/Update/Delete Queries */
+        protected void ExecuteEditQuery(string query, SqlParameter[] sqlParameters)
+        {
+            SqlCommand command = new SqlCommand();
+
+            try
+            {
+                command.Connection = OpenConnection();
+                command.CommandText = query;
+                command.Parameters.AddRange(sqlParameters);
+                _adapter.InsertCommand = command;
+                command.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                // Print.ErrorLog(e);
+                throw;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        /* For Select Queries */
+        protected DataTable ExecuteSelectQuery(string query, params SqlParameter[] sqlParameters)
+        {
+            SqlCommand command = new SqlCommand();
+            DataTable dataTable;
+            DataSet dataSet = new DataSet();
+
+            try
+            {
+                command.Connection = OpenConnection();
+                command.CommandText = query;
+                command.Parameters.AddRange(sqlParameters);
+                command.ExecuteNonQuery();
+                _adapter.SelectCommand = command;
+                _adapter.Fill(dataSet);
+                dataTable = dataSet.Tables[0];
+            }
+            catch (SqlException e)
+            {
+                // Print.ErrorLog(e);
+                throw;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+
+            return dataTable;
+        }
+    }
+}
+
