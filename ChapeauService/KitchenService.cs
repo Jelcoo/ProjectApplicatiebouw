5d347645f020263f155d84ef030084d90dff1edc
Iolite312 <leonwatertor18@gmail.com>
2024-05-14 20:30:07 +0200

    Added a kitchen service

---
+++ ChapeauService/KitchenService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChapeauModel;
+using ChapeauDAL;
+
+namespace ChapeauService
+{
+    public class KitchenService
+    {
+        private KitchenDao _kitchenDao;
+
+        public KitchenService()
+        {
+            _kitchenDao = new KitchenDao();
+        }
+
+        public List<Order> GetOrdersInOrder()
+        {
+            return _kitchenDao.GetOrdersInOrder();
+        }
+        public void UpdateOrderLinesStatus(Order order)
+        {
+            _kitchenDao.UpdateOrderLinesStatus(order);
+        }
+        public List<Order> GetPreviousCompletedOrders()
+        {
+            return _kitchenDao.GetPreviousCompletedOrders();
+        }
+    }
+}
