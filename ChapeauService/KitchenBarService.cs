2bdc2ba70e1fce39a558d0ee05dc77dea783dbe3
iolite312 <leonwatertor18@gmail.com>
2024-05-15 12:20:04 +0200

    Renamed and made the kitchenDao a generic one for the bar and the kitchen because they use the same code

---
+++ ChapeauService/KitchenBarService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChapeauModel;
+using ChapeauDAL;
+using ChapeauModel.Enums;
+
+namespace ChapeauService
+{
+    public class KitchenBarService
+    {
+        private KitchenBarDao _kitchenBarDao;
+
+        public KitchenBarService()
+        {
+            _kitchenBarDao = new KitchenBarDao();
+        }
+
+        public List<Order> GetOrdersInOrder(EOrderDestination orderType)
+        {
+            return _kitchenBarDao.GetOrdersInOrder(orderType);
+        }
+        public void UpdateOrderLinesStatus(Order order)
+        {
+            _kitchenBarDao.UpdateOrderLinesStatus(order);
+        }
+        public List<Order> GetPreviousCompletedOrders(EOrderDestination orderType)
+        {
+            return _kitchenBarDao.GetPreviousCompletedOrders(orderType);
+        }
+    }
+}
