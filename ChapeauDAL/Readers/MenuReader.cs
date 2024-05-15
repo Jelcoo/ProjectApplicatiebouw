d86d6865671053f9c46089e9e237034db1189a50
iolite312 <leonwatertor18@gmail.com>
2024-05-15 12:28:16 +0200

    Removed Detailed reader

--- ChapeauDAL/Readers/MenuReader.cs
+++ ChapeauDAL/Readers/MenuReader.cs
@@ -25,20 +25,6 @@ namespace ChapeauDAL.Readers
                 menuId: (int)reader["menuId"],
                 menuTypeId: reader["orderNoteId"] == DBNull.Value ? null : (int)reader["menuTypeId"],
                 name: (string)reader["itemName"],
-                VATRate: (double)reader["VATRate"],
-                price: (double)reader["price"]
-            );
-
-            return menuItem;
-        }
-        public static MenuItem ReadMenuItemDetailed(SqlDataReader reader)
-        {
-            MenuItem menuItem = new MenuItem(
-                menuItemId: (int)reader["menuItemId"],
-                stockId: (int)reader["stockId"],
-                menuId: (int)reader["menuId"],
-                menuTypeId: reader["orderNoteId"] == DBNull.Value ? null : (int)reader["menuTypeId"],
-                name: (string)reader["itemDetailName"],
                 detailName: (string)reader["itemDetailName"],
                 VATRate: (double)reader["VATRate"],
                 price: (double)reader["price"]
