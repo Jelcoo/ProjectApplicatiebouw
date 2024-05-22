b0900d21b5ef5a8f8a8eba82572ab7ce592cc63e
Jelco <53396500+Jelcoo@users.noreply.github.com>
2024-05-22 11:40:10 +0200

    Add basic layout

---
+++ ChapeauUI/Helpers/GenericHelpers.cs
@@ -0,0 +1,11 @@
+﻿namespace ChapeauUI.Helpers
+{
+    public static class GenericHelpers
+    {
+        public static string FormatDateTime(DateTime dateTime)
+        {
+            // Format dateTime to <month> <day> (<day>) | <time>
+            return dateTime.ToString("MMMM dd (ddd) | HH:mm");
+        }
+    }
+}
---
+++ ChapeauUI/OrderUI/OrderHome.cs
@@ -0,0 +1,17 @@
+﻿using ChapeauUI.Helpers;
+
+namespace ChapeauUI.OrderUI
+{
+    public partial class OrderHome : Form
+    {
+        public OrderHome()
+        {
+            InitializeComponent();
+        }
+
+        private void OrderHome_Load(object sender, EventArgs e)
+        {
+            dateTimeLabel.Text = GenericHelpers.FormatDateTime(DateTime.Now);
+        }
+    }
+}
