6da576fa5a196d96fee4067bf5bf35346d351a36
Jelco <53396500+Jelcoo@users.noreply.github.com>
2024-05-27 09:28:22 +0200

    Add menu selector

---
+++ ChapeauModel/Enums/EMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauModel.Enums
+{
+    public enum EMenu
+    {
+        Drinks = 1, Lunch, Dinner
+    }
+}
--- ChapeauUI/OrderUI/OrderHome.cs
+++ ChapeauUI/OrderUI/OrderHome.cs
@@ -1,9 +1,13 @@
-﻿using ChapeauUI.Helpers;
+﻿using ChapeauModel.Enums;
+using ChapeauUI.Components;
+using ChapeauUI.Helpers;
 
 namespace ChapeauUI.OrderUI
 {
     public partial class OrderHome : Form
     {
+        private EMenu _selectedMenu = EMenu.Drinks;
+
         public OrderHome()
         {
             InitializeComponent();
@@ -12,6 +16,59 @@ namespace ChapeauUI.OrderUI
         private void OrderHome_Load(object sender, EventArgs e)
         {
             dateTimeLabel.Text = GenericHelpers.FormatDateTime(DateTime.Now);
+            UpdateMenuStyles();
+        }
+
+        private void UpdateMenuStyles()
+        {
+            if (_selectedMenu == EMenu.Drinks)
+            {
+                SelectMenuStyle(menuSelectorDrinks);
+                UnselectMenuStyle(menuSelectorLunch);
+                UnselectMenuStyle(menuSelectorDinner);
+            }
+            if (_selectedMenu == EMenu.Lunch)
+            {
+                UnselectMenuStyle(menuSelectorDrinks);
+                SelectMenuStyle(menuSelectorLunch);
+                UnselectMenuStyle(menuSelectorDinner);
+            }
+            if (_selectedMenu == EMenu.Dinner)
+            {
+                UnselectMenuStyle(menuSelectorDrinks);
+                UnselectMenuStyle(menuSelectorLunch);
+                SelectMenuStyle(menuSelectorDinner);
+            }
+        }
+        private void SelectMenuStyle(RoundedButton buutton)
+        {
+            buutton.BackColor = Color.FromArgb(246, 255, 248);
+            buutton.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Underline, GraphicsUnit.Point);
+            buutton.HasBorder = true;
+        }
+        private void UnselectMenuStyle(RoundedButton buutton)
+        {
+            buutton.BackColor = Color.FromArgb(246, 255, 248);
+            buutton.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Underline, GraphicsUnit.Point);
+            buutton.HasBorder = false;
+        }
+
+        private void menuSelectorDrinks_Click(object sender, EventArgs e)
+        {
+            _selectedMenu = EMenu.Drinks;
+            UpdateMenuStyles();
+        }
+
+        private void menuSelectorLunch_Click(object sender, EventArgs e)
+        {
+            _selectedMenu = EMenu.Lunch;
+            UpdateMenuStyles();
+        }
+
+        private void menuSelectorDinner_Click(object sender, EventArgs e)
+        {
+            _selectedMenu = EMenu.Dinner;
+            UpdateMenuStyles();
         }
     }
 }
