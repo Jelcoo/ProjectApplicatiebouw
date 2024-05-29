7215cc1ecf87211e9308ebc7ce0c1543dedf4f2c
iolite312 <leonwatertor18@gmail.com>
2024-05-29 15:26:55 +0200

    The complete order component

---
+++ ChapeauUI/Components/CompleteOrderTemplate.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ChapeauUI.Components
+{
+    partial class CompleteOrderTemplate
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            orderedAtLabel = new Label();
+            LayoutOrderPanel = new TableLayoutPanel();
+            ReadyButton = new Button();
+            SuspendLayout();
+            // 
+            // orderedAtLabel
+            // 
+            orderedAtLabel.AutoSize = true;
+            orderedAtLabel.Location = new Point(30, 31);
+            orderedAtLabel.Name = "orderedAtLabel";
+            orderedAtLabel.Size = new Size(59, 25);
+            orderedAtLabel.TabIndex = 0;
+            orderedAtLabel.Text = "label1";
+            // 
+            // LayoutOrderPanel
+            // 
+            LayoutOrderPanel.AutoSize = true;
+            LayoutOrderPanel.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            LayoutOrderPanel.ColumnCount = 1;
+            LayoutOrderPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            LayoutOrderPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            LayoutOrderPanel.Location = new Point(10, 119);
+            LayoutOrderPanel.Name = "LayoutOrderPanel";
+            LayoutOrderPanel.RowCount = 1;
+            LayoutOrderPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            LayoutOrderPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            LayoutOrderPanel.Size = new Size(0, 0);
+            LayoutOrderPanel.TabIndex = 3;
+            // 
+            // ReadyButton
+            // 
+            ReadyButton.Location = new Point(30, 355);
+            ReadyButton.Name = "ReadyButton";
+            ReadyButton.Size = new Size(197, 87);
+            ReadyButton.TabIndex = 4;
+            ReadyButton.Text = "Ready?";
+            ReadyButton.UseVisualStyleBackColor = true;
+            ReadyButton.Click += ReadyButton_Click;
+            // 
+            // CompleteOrderTemplate
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            BackColor = Color.White;
+            Controls.Add(ReadyButton);
+            Controls.Add(LayoutOrderPanel);
+            Controls.Add(orderedAtLabel);
+            Name = "CompleteOrderTemplate";
+            Padding = new Padding(10);
+            Size = new Size(240, 455);
+            Load += CompleteOrderTemplate_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label orderedAtLabel;
+        private TableLayoutPanel LayoutOrderPanel;
+        private Button ReadyButton;
+    }
+}
---
+++ ChapeauUI/Components/CompleteOrderTemplate.cs
@@ -0,0 +1,38 @@
+﻿using ChapeauModel;
+using ChapeauModel.Enums;
+using ChapeauService;
+
+namespace ChapeauUI.Components
+{
+    public partial class CompleteOrderTemplate : UserControl
+    {
+        private Order _order;
+        private KitchenBarService _kitchenBarService;
+        private MenuChecklist _checklist;
+        public CompleteOrderTemplate(Order order)
+        {
+            InitializeComponent();
+            _order = order;
+            _kitchenBarService = new KitchenBarService();
+        }
+
+        private void CompleteOrderTemplate_Load(object sender, EventArgs e)
+        {
+            orderedAtLabel.Text = $"Ordered at: {_order.OrderedAt.ToString("t")}";
+            _checklist = new MenuChecklist(_order.OrderLines);
+            LayoutOrderPanel.Controls.Add(_checklist);
+        }
+
+        private void ReadyButton_Click(object sender, EventArgs e)
+        {
+            List<OrderLine> lines = _checklist.GetUpdatedOrderLines();
+            if (lines.Count == 0) return; 
+            foreach (OrderLine line in lines) 
+            {
+                line.OrderLineStatus = EOrderLineStatus.Ready;
+            }
+            _order.SetOrderLines(lines);
+            _kitchenBarService.UpdateOrderLinesStatus(_order);
+        }
+    }
+}
