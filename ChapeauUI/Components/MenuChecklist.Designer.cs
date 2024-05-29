6348dcd7e3042d6422b71fa8835f1860430d56a6
iolite312 <leonwatertor18@gmail.com>
2024-05-29 13:54:06 +0200

    Checklist for the items

---
+++ ChapeauUI/Components/MenuChecklist.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ChapeauUI.Components
+{
+    partial class MenuChecklist
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkedListBox1 = new CheckedListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(0, 0);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(355, 256);
+            checkedListBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(48, 386);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // MenuChecklist
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button1);
+            Controls.Add(checkedListBox1);
+            Name = "MenuChecklist";
+            Size = new Size(593, 622);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private CheckedListBox checkedListBox1;
+        private Button button1;
+    }
+}
