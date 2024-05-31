969ce21dfd1f32b331255fa3951b3976f2ce33b9
Iolite312 <leonwatertor18@gmail.com>
2024-05-31 11:48:50 +0200

    Checking for the preparing button + refresh logic

--- ChapeauUI/Components/CompleteOrderTemplate.cs
+++ ChapeauUI/Components/CompleteOrderTemplate.cs
@@ -1,6 +1,7 @@
 ﻿using ChapeauModel;
 using ChapeauModel.Enums;
 using ChapeauService;
+using ChapeauUI.KitchenUI;
 
 namespace ChapeauUI.Components
 {
@@ -10,33 +11,47 @@ namespace ChapeauUI.Components
         private KitchenBarService _kitchenBarService;
         private MenuChecklist _checklist;
         private EOrderTime _orderTime;
+        private KitchenHome _form;
+        public int ChecklistCount { get; private set; }
 
-        public CompleteOrderTemplate(Order order, EOrderTime orderTime)
+        public CompleteOrderTemplate(Order order, EOrderTime orderTime, KitchenHome form)
         {
             InitializeComponent();
             _order = order;
             _orderTime = orderTime;
             _kitchenBarService = new KitchenBarService();
+            _form = form;
+            _checklist = new MenuChecklist(_order.OrderLines, _orderTime);
+            ChecklistCount = _checklist.ChecklistCount;
         }
 
         private void CompleteOrderTemplate_Load(object sender, EventArgs e)
         {
             orderedAtLabel.Text = $"Ordered at: {_order.OrderedAt.ToString("t")}";
-            _checklist = new MenuChecklist(_order.OrderLines, _orderTime);
             LayoutOrderPanel.Controls.Add(_checklist);
             if (_orderTime == EOrderTime.InThePast) 
             {
                 StartPreparingBTN.Enabled = false;
                 ReadyButton.Enabled = false;
             }
+            if (CheckPreparingStatus())
+            {
+                StartPreparingBTN.Enabled = false;
+            }
         }
 
         private void ReadyButton_Click(object sender, EventArgs e)
         {
+            if (StartPreparingBTN.Enabled)
+            {
+                MessageBox.Show("Please click start preparing first");
+                return;
+            }
             List<OrderLine> lines = _checklist.GetUpdatedOrderLines();
             if (lines.Count == 0) 
             {
                 MessageBox.Show("Please select some items that are done!");
+                return;
             }
             foreach (OrderLine line in lines)
             {
@@ -44,6 +59,7 @@ namespace ChapeauUI.Components
             }
             _order.SetOrderLines(lines);
             _kitchenBarService.UpdateOrderLinesStatus(_order);
+            _form.Addpanel(EOrderTime.Current);
         }
 
         private void StartPreparingBTN_Click(object sender, EventArgs e)
@@ -56,6 +72,16 @@ namespace ChapeauUI.Components
             _order.SetOrderLines(lines);
             _kitchenBarService.UpdateOrderLinesStatus(_order);
             StartPreparingBTN.Enabled = false;
+            _form.Addpanel(EOrderTime.Current);
+        }
+        private bool CheckPreparingStatus()
+        {
+            int preparing = 0;
+            foreach (OrderLine line in _order.OrderLines)
+            {
+                if (line.OrderLineStatus == EOrderLineStatus.Preparing) preparing++;
+            }
+            return preparing > 0;
         }
     }
 }
