4d1ec3e782a204319ea993307c5d59c34ffccf55
Iolite312 <leonwatertor18@gmail.com>
2024-05-31 10:54:52 +0200

    Update button to say your starting preparing

--- ChapeauUI/Components/CompleteOrderTemplate.Designer.cs
+++ ChapeauUI/Components/CompleteOrderTemplate.Designer.cs
@@ -31,14 +31,16 @@
             orderedAtLabel = new Label();
             LayoutOrderPanel = new TableLayoutPanel();
             ReadyButton = new Button();
+            StartPreparingBTN = new Button();
             SuspendLayout();
             // 
             // orderedAtLabel
             // 
             orderedAtLabel.AutoSize = true;
-            orderedAtLabel.Location = new Point(30, 31);
+            orderedAtLabel.Location = new Point(24, 25);
+            orderedAtLabel.Margin = new Padding(2, 0, 2, 0);
             orderedAtLabel.Name = "orderedAtLabel";
-            orderedAtLabel.Size = new Size(59, 25);
+            orderedAtLabel.Size = new Size(50, 20);
             orderedAtLabel.TabIndex = 0;
             orderedAtLabel.Text = "label1";
             // 
@@ -48,38 +50,52 @@
             LayoutOrderPanel.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             LayoutOrderPanel.ColumnCount = 1;
             LayoutOrderPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-            LayoutOrderPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            LayoutOrderPanel.Location = new Point(10, 119);
+            LayoutOrderPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 16F));
+            LayoutOrderPanel.Location = new Point(8, 95);
+            LayoutOrderPanel.Margin = new Padding(2);
             LayoutOrderPanel.Name = "LayoutOrderPanel";
             LayoutOrderPanel.RowCount = 1;
             LayoutOrderPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            LayoutOrderPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            LayoutOrderPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 1F));
             LayoutOrderPanel.Size = new Size(0, 0);
             LayoutOrderPanel.TabIndex = 3;
             // 
             // ReadyButton
             // 
-            ReadyButton.Location = new Point(30, 355);
+            ReadyButton.Location = new Point(81, 284);
+            ReadyButton.Margin = new Padding(2);
             ReadyButton.Name = "ReadyButton";
-            ReadyButton.Size = new Size(197, 87);
+            ReadyButton.Size = new Size(158, 70);
             ReadyButton.TabIndex = 4;
             ReadyButton.Text = "Ready?";
             ReadyButton.UseVisualStyleBackColor = true;
             ReadyButton.Click += ReadyButton_Click;
             // 
+            // StartPreparingBTN
+            // 
+            StartPreparingBTN.Location = new Point(179, 25);
+            StartPreparingBTN.Name = "StartPreparingBTN";
+            StartPreparingBTN.Size = new Size(127, 29);
+            StartPreparingBTN.TabIndex = 5;
+            StartPreparingBTN.Text = "Start preparing";
+            StartPreparingBTN.UseVisualStyleBackColor = true;
+            StartPreparingBTN.Click += StartPreparingBTN_Click;
+            // 
             // CompleteOrderTemplate
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
             BackColor = Color.White;
+            Controls.Add(StartPreparingBTN);
             Controls.Add(ReadyButton);
             Controls.Add(LayoutOrderPanel);
             Controls.Add(orderedAtLabel);
+            Margin = new Padding(2);
             Name = "CompleteOrderTemplate";
-            Padding = new Padding(10);
-            Size = new Size(240, 455);
+            Padding = new Padding(8);
+            Size = new Size(317, 364);
             Load += CompleteOrderTemplate_Load;
             ResumeLayout(false);
             PerformLayout();
@@ -90,5 +106,6 @@
         private Label orderedAtLabel;
         private TableLayoutPanel LayoutOrderPanel;
         private Button ReadyButton;
+        private Button StartPreparingBTN;
     }
 }
--- ChapeauUI/Components/CompleteOrderTemplate.cs
+++ ChapeauUI/Components/CompleteOrderTemplate.cs
@@ -9,30 +9,48 @@ namespace ChapeauUI.Components
         private Order _order;
         private KitchenBarService _kitchenBarService;
         private MenuChecklist _checklist;
-        public CompleteOrderTemplate(Order order)
+        private EOrderTime _orderTime;
+        public CompleteOrderTemplate(Order order, EOrderTime orderTime)
         {
             InitializeComponent();
             _order = order;
+            _orderTime = orderTime;
             _kitchenBarService = new KitchenBarService();
         }
 
         private void CompleteOrderTemplate_Load(object sender, EventArgs e)
         {
             orderedAtLabel.Text = $"Ordered at: {_order.OrderedAt.ToString("t")}";
-            _checklist = new MenuChecklist(_order.OrderLines);
+            _checklist = new MenuChecklist(_order.OrderLines, _orderTime);
             LayoutOrderPanel.Controls.Add(_checklist);
+            if (_orderTime == EOrderTime.InThePast) StartPreparingBTN.Enabled = false;
         }
 
         private void ReadyButton_Click(object sender, EventArgs e)
         {
             List<OrderLine> lines = _checklist.GetUpdatedOrderLines();
-            if (lines.Count == 0) return; 
-            foreach (OrderLine line in lines) 
+            if (lines.Count == 0) 
+            {
+                MessageBox.Show("Please select some items that are done!");
+            }
+            foreach (OrderLine line in lines)
             {
                 line.OrderLineStatus = EOrderLineStatus.Ready;
             }
             _order.SetOrderLines(lines);
             _kitchenBarService.UpdateOrderLinesStatus(_order);
+        }
+
+        private void StartPreparingBTN_Click(object sender, EventArgs e)
+        {
+            List<OrderLine> lines = _order.OrderLines;
+            foreach (OrderLine line in lines)
+            {
+                line.OrderLineStatus = EOrderLineStatus.Preparing;
+            }
+            _order.SetOrderLines(lines);
+            _kitchenBarService.UpdateOrderLinesStatus(_order);
+            StartPreparingBTN.Enabled = false;
         }
     }
 }
