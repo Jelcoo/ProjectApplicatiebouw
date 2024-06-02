038700a16287e99cb8d8020466ac9707028afc64
Jelco <53396500+Jelcoo@users.noreply.github.com>
2024-06-02 15:41:29 +0200

    Add home screen

--- ChapeauUI/ChapeauPanel.Designer.cs
+++ ChapeauUI/ChapeauPanel.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            orderButton = new Button();
+            kitchenButton = new Button();
+            barOverview = new Button();
+            managementButton = new Button();
+            paymentButton = new Button();
+            SuspendLayout();
+            // 
+            // orderButton
+            // 
+            orderButton.Location = new Point(12, 12);
+            orderButton.Name = "orderButton";
+            orderButton.Size = new Size(150, 75);
+            orderButton.TabIndex = 0;
+            orderButton.Text = "Ordering";
+            orderButton.UseVisualStyleBackColor = true;
+            orderButton.Click += orderButton_Click;
+            // 
+            // kitchenButton
+            // 
+            kitchenButton.Location = new Point(168, 12);
+            kitchenButton.Name = "kitchenButton";
+            kitchenButton.Size = new Size(150, 75);
+            kitchenButton.TabIndex = 1;
+            kitchenButton.Text = "Kitchen overview";
+            kitchenButton.UseVisualStyleBackColor = true;
+            kitchenButton.Click += kitchenButton_Click;
+            // 
+            // barOverview
+            // 
+            barOverview.Location = new Point(324, 12);
+            barOverview.Name = "barOverview";
+            barOverview.Size = new Size(150, 75);
+            barOverview.TabIndex = 2;
+            barOverview.Text = "Bar overview";
+            barOverview.UseVisualStyleBackColor = true;
+            barOverview.Click += barOverview_Click;
+            // 
+            // managementButton
+            // 
+            managementButton.Location = new Point(480, 12);
+            managementButton.Name = "managementButton";
+            managementButton.Size = new Size(150, 75);
+            managementButton.TabIndex = 3;
+            managementButton.Text = "Management";
+            managementButton.UseVisualStyleBackColor = true;
+            managementButton.Click += managementButton_Click;
+            // 
+            // paymentButton
+            // 
+            paymentButton.Location = new Point(636, 12);
+            paymentButton.Name = "paymentButton";
+            paymentButton.Size = new Size(150, 75);
+            paymentButton.TabIndex = 4;
+            paymentButton.Text = "Payments";
+            paymentButton.UseVisualStyleBackColor = true;
+            paymentButton.Click += paymentButton_Click;
+            // 
+            // ChapeauPanel
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(793, 103);
+            Controls.Add(paymentButton);
+            Controls.Add(managementButton);
+            Controls.Add(barOverview);
+            Controls.Add(kitchenButton);
+            Controls.Add(orderButton);
+            Name = "ChapeauPanel";
+            Text = "Chapeau Home";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button orderButton;
+        private Button kitchenButton;
+        private Button barOverview;
+        private Button managementButton;
+        private Button paymentButton;
     }
 }
--- ChapeauUI/ChapeauPanel.cs
+++ ChapeauUI/ChapeauPanel.cs
@@ -1,3 +1,7 @@
+using ChapeauUI.BarUI;
+using ChapeauUI.KitchenUI;
+using ChapeauUI.TableUI;
+
 namespace ChapeauUI
 {
     public partial class ChapeauPanel : Form
@@ -5,6 +9,37 @@ namespace ChapeauUI
         public ChapeauPanel()
         {
             InitializeComponent();
+        }
+
+        private void orderButton_Click(object sender, EventArgs e)
+        {
+            TableHome tableHome = new TableHome();
+            tableHome.Show();
+            this.Hide();
+        }
+
+        private void kitchenButton_Click(object sender, EventArgs e)
+        {
+            KitchenHome kitchenHome = new KitchenHome();
+            kitchenHome.Show();
+            this.Hide();
+        }
+
+        private void barOverview_Click(object sender, EventArgs e)
+        {
+            BarHome barHome = new BarHome();
+            barHome.Show();
+            this.Hide();
+        }
+
+        private void managementButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void paymentButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
