5caaf14c2622b1f002d8d5f064929b4ee4e6a807
iolite312 <leonwatertor18@gmail.com>
2024-06-03 11:57:05 +0200

    Fixed a scaling issue

--- ChapeauUI/BarUI/BarHome.Designer.cs
+++ ChapeauUI/BarUI/BarHome.Designer.cs
@@ -34,10 +34,10 @@
             loggedInAsPreLabel = new Label();
             chapeauLogo = new PictureBox();
             backgroundPanel = new Panel();
+            backButton = new Button();
             CompletedOrderButton = new Components.RoundedButton();
             currentOrderButton = new Components.RoundedButton();
             barOrderLayoutPanel = new TableLayoutPanel();
-            backButton = new Button();
             ((System.ComponentModel.ISupportInitialize)chapeauLogo).BeginInit();
             backgroundPanel.SuspendLayout();
             SuspendLayout();
@@ -46,9 +46,10 @@
             // 
             dateTimeLabel.AutoSize = true;
             dateTimeLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            dateTimeLabel.Location = new Point(1177, 8);
+            dateTimeLabel.Location = new Point(1471, 10);
+            dateTimeLabel.Margin = new Padding(4, 0, 4, 0);
             dateTimeLabel.Name = "dateTimeLabel";
-            dateTimeLabel.Size = new Size(188, 28);
+            dateTimeLabel.Size = new Size(230, 32);
             dateTimeLabel.TabIndex = 14;
             dateTimeLabel.Text = "Loading date & time...";
             dateTimeLabel.TextAlign = ContentAlignment.MiddleRight;
@@ -57,9 +58,10 @@
             // 
             loggedInAsLabel.AutoSize = true;
             loggedInAsLabel.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            loggedInAsLabel.Location = new Point(468, 9);
+            loggedInAsLabel.Location = new Point(585, 11);
+            loggedInAsLabel.Margin = new Padding(4, 0, 4, 0);
             loggedInAsLabel.Name = "loggedInAsLabel";
-            loggedInAsLabel.Size = new Size(61, 28);
+            loggedInAsLabel.Size = new Size(73, 32);
             loggedInAsLabel.TabIndex = 13;
             loggedInAsLabel.Text = "Mark";
             // 
@@ -67,18 +69,20 @@
             // 
             loggedInAsPreLabel.AutoSize = true;
             loggedInAsPreLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            loggedInAsPreLabel.Location = new Point(192, 8);
+            loggedInAsPreLabel.Location = new Point(240, 10);
+            loggedInAsPreLabel.Margin = new Padding(4, 0, 4, 0);
             loggedInAsPreLabel.Name = "loggedInAsPreLabel";
-            loggedInAsPreLabel.Size = new Size(270, 28);
+            loggedInAsPreLabel.Size = new Size(332, 32);
             loggedInAsPreLabel.TabIndex = 12;
             loggedInAsPreLabel.Text = "You are currently logged in as";
             // 
             // chapeauLogo
             // 
             chapeauLogo.Image = (Image)resources.GetObject("chapeauLogo.Image");
-            chapeauLogo.Location = new Point(22, 27);
+            chapeauLogo.Location = new Point(28, 34);
+            chapeauLogo.Margin = new Padding(4, 4, 4, 4);
             chapeauLogo.Name = "chapeauLogo";
-            chapeauLogo.Size = new Size(130, 130);
+            chapeauLogo.Size = new Size(162, 162);
             chapeauLogo.TabIndex = 11;
             chapeauLogo.TabStop = false;
             // 
@@ -89,10 +93,22 @@
             backgroundPanel.Controls.Add(CompletedOrderButton);
             backgroundPanel.Controls.Add(currentOrderButton);
             backgroundPanel.Controls.Add(barOrderLayoutPanel);
-            backgroundPanel.Location = new Point(192, 57);
+            backgroundPanel.Location = new Point(240, 71);
+            backgroundPanel.Margin = new Padding(4, 4, 4, 4);
             backgroundPanel.Name = "backgroundPanel";
-            backgroundPanel.Size = new Size(1230, 954);
+            backgroundPanel.Size = new Size(1538, 1192);
             backgroundPanel.TabIndex = 10;
+            // 
+            // backButton
+            // 
+            backButton.Location = new Point(1705, 26);
+            backButton.Margin = new Padding(4, 4, 4, 4);
+            backButton.Name = "backButton";
+            backButton.Size = new Size(171, 69);
+            backButton.TabIndex = 3;
+            backButton.Text = "< Back";
+            backButton.UseVisualStyleBackColor = true;
+            backButton.Click += backButton_Click;
             // 
             // CompletedOrderButton
             // 
@@ -100,15 +116,16 @@
             CompletedOrderButton.BorderColor = Color.Black;
             CompletedOrderButton.BorderRadius = 5;
             CompletedOrderButton.BorderWidth = 5;
+            CompletedOrderButton.Cursor = Cursors.Hand;
             CompletedOrderButton.HasBorder = false;
-            CompletedOrderButton.Location = new Point(196, 17);
+            CompletedOrderButton.Location = new Point(245, 21);
+            CompletedOrderButton.Margin = new Padding(4, 4, 4, 4);
             CompletedOrderButton.Name = "CompletedOrderButton";
-            CompletedOrderButton.Size = new Size(174, 44);
+            CompletedOrderButton.Size = new Size(218, 55);
             CompletedOrderButton.TabIndex = 2;
             CompletedOrderButton.Text = "Completed Orders";
             CompletedOrderButton.UseVisualStyleBackColor = false;
             CompletedOrderButton.Click += roundedButton2_Click;
-            CompletedOrderButton.Cursor = Cursors.Hand;
             // 
             // currentOrderButton
             // 
@@ -116,16 +133,17 @@
             currentOrderButton.BorderColor = Color.Black;
             currentOrderButton.BorderRadius = 5;
             currentOrderButton.BorderWidth = 5;
+            currentOrderButton.Cursor = Cursors.Hand;
             currentOrderButton.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
             currentOrderButton.HasBorder = false;
-            currentOrderButton.Location = new Point(16, 17);
+            currentOrderButton.Location = new Point(20, 21);
+            currentOrderButton.Margin = new Padding(4, 4, 4, 4);
             currentOrderButton.Name = "currentOrderButton";
-            currentOrderButton.Size = new Size(174, 44);
+            currentOrderButton.Size = new Size(218, 55);
             currentOrderButton.TabIndex = 1;
             currentOrderButton.Text = "Current Orders";
             currentOrderButton.UseVisualStyleBackColor = false;
             currentOrderButton.Click += currentOrderButton_Click;
-            currentOrderButton.Cursor = Cursors.Hand;
             // 
             // barOrderLayoutPanel
             // 
@@ -135,34 +153,26 @@
             barOrderLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
             barOrderLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
             barOrderLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
-            barOrderLayoutPanel.Location = new Point(16, 67);
+            barOrderLayoutPanel.Location = new Point(20, 84);
+            barOrderLayoutPanel.Margin = new Padding(4, 4, 4, 4);
             barOrderLayoutPanel.Name = "barOrderLayoutPanel";
             barOrderLayoutPanel.RowCount = 1;
             barOrderLayoutPanel.RowStyles.Add(new RowStyle());
-            barOrderLayoutPanel.Size = new Size(1185, 653);
+            barOrderLayoutPanel.Size = new Size(1481, 1088);
             barOrderLayoutPanel.TabIndex = 0;
-            // 
-            // backButton
-            // 
-            backButton.Location = new Point(1364, 21);
-            backButton.Name = "backButton";
-            backButton.Size = new Size(137, 55);
-            backButton.TabIndex = 3;
-            backButton.Text = "< Back";
-            backButton.UseVisualStyleBackColor = true;
-            backButton.Click += backButton_Click;
             // 
             // BarHome
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(164, 195, 178);
-            ClientSize = new Size(1444, 1018);
+            ClientSize = new Size(1805, 1272);
             Controls.Add(dateTimeLabel);
             Controls.Add(loggedInAsLabel);
             Controls.Add(loggedInAsPreLabel);
             Controls.Add(chapeauLogo);
             Controls.Add(backgroundPanel);
+            Margin = new Padding(4, 4, 4, 4);
             Name = "BarHome";
             Text = "BarHome";
             Load += BarHome_Load;
