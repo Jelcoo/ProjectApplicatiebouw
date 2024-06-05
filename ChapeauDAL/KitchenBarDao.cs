52db47f3f42bc7668bb8fdaacdfeae1bb20abfe5
iolite312 <leonwatertor18@gmail.com>
2024-06-05 18:50:32 +0200

    Reworked one query

--- ChapeauDAL/KitchenBarDao.cs
+++ ChapeauDAL/KitchenBarDao.cs
@@ -9,10 +9,7 @@ namespace ChapeauDAL
     {
         public List<Order> GetOrdersInOrder(EOrderDestination orderType)
         {
-            string query;
-            if (orderType == EOrderDestination.Kitchen)
-            {
-                query = @"
+            string query = $@"
 SELECT O.orderId, O.invoiceId, O.orderedAt, OL.orderLineId, OL.quantity, OS.orderStatusId, OS.[status], MI.menuItemId, MI.stockId, ST.[count], MI.menuId, MI.itemDetailName, MI.itemName, MI.VATRate, MI.price, MT.menuTypeId, MT.typeName, [ON].orderNoteId, [ON].note 
 FROM orders AS O 
 JOIN orderLines AS OL ON OL.orderId = O.orderId 
@@ -21,24 +18,10 @@ JOIN menuItems AS MI ON OL.menuItemId = MI.menuItemId
 JOIN stock AS ST ON MI.stockId = ST.stockId
 LEFT JOIN menuTypes AS MT ON MT.menuTypeId = MI.menuTypeId 
 LEFT JOIN orderNotes AS [ON] ON [ON].orderLineId = OL.orderLineId 
-WHERE MT.menuTypeId IS NOT NULL 
+WHERE MT.menuTypeId {(orderType == EOrderDestination.Kitchen ? "IS NOT NULL" : "IS NULL")} 
 AND CONVERT(date, O.orderedAt) = CONVERT(date, GETDATE()) 
 ORDER BY O.orderedAt";
-            }
-            else {
-                query = @"
-SELECT O.orderId, O.invoiceId, O.orderedAt, OL.orderLineId, OL.quantity, OS.orderStatusId, OS.[status], MI.menuItemId, MI.stockId, ST.[count], MI.menuId, MI.itemDetailName, MI.itemName, MI.VATRate, MI.price, MT.menuTypeId, MT.typeName, [ON].orderNoteId, [ON].note 
-FROM orders AS O 
-JOIN orderLines AS OL ON OL.orderId = O.orderId 
-JOIN orderStatuses AS OS ON OL.orderStatusId = OS.orderStatusId 
-JOIN menuItems AS MI ON OL.menuItemId = MI.menuItemId
-JOIN stock AS ST ON MI.stockId = ST.stockId
-LEFT JOIN menuTypes AS MT ON MT.menuTypeId = MI.menuTypeId 
-LEFT JOIN orderNotes AS [ON] ON [ON].orderLineId = OL.orderLineId 
-WHERE MT.menuTypeId IS NULL 
-AND CONVERT(date, O.orderedAt) = CONVERT(date, GETDATE()) 
-ORDER BY O.orderedAt";
-            }
+
             SqlCommand command = new SqlCommand(query, OpenConnection());
 
             SqlDataReader reader = command.ExecuteReader();
